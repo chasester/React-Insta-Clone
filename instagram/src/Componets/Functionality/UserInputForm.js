c7bd092fd50080470c269d85ce259b732530fdde
Chase Wex <clapingmanly@hotmail.com>
2019-04-30 17:04:54 -0400

    Updated user input form to fix some errors

--- instagram/src/Componets/Functionality/UserInputForm.js
+++ instagram/src/Componets/Functionality/UserInputForm.js
@@ -13,20 +13,21 @@ class UserInputForm extends React.Component
     handleReset(event){
         event.preventDefault();
         this.setState({value: ''});
-        if(this.props.resetCb) this.props.resetCb()
+        if(this.props.resetCb) this.props.resetCb();
+        if(this.props.changeCb) this.props.changeCb("");
     }
     handleChange(event) 
     {
         this.setState({value: event.target.value});
-        event.preventDefault();  
-        if(this.props.changeCb) this.props.changeCb()
+        event.preventDefault();
+        if(this.props.changeCb) this.props.changeCb(event.target.value);
     }
 
     handleSumbit(event)
     {
         event.preventDefault();
         if(this.props.submitCb) this.props.submitCb(this.state.value);
-        if(this.props.clearOnSubmit)this.setState({value: ''});
+        if(this.props.clearOnSubmit) this.setState({value: ''}); 
     }
     render()
     {
