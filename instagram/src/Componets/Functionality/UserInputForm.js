107023923eaaf714919ad22063740197ec586d2f
Chase Wex <clapingmanly@hotmail.com>
2019-05-01 19:35:58 -0400

    Updated userforums to help with new functionality for the login

--- instagram/src/Componets/Functionality/UserInputForm.js
+++ instagram/src/Componets/Functionality/UserInputForm.js
@@ -5,10 +5,16 @@ class UserInputForm extends React.Component
     constructor(props) {
         super(props);
         this.state = {value: ''};
+        this.clear = this.props.clear;
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSumbit = this.handleSumbit.bind(this);
         this.handleReset = this.handleReset.bind(this);
+    }
+
+    componentWillUpdate()
+    {
+
     }
     handleReset(event){
         event.preventDefault();
@@ -27,15 +33,15 @@ class UserInputForm extends React.Component
     {
         event.preventDefault();
         if(this.props.submitCb) this.props.submitCb(this.state.value);
-        if(this.props.clearOnSubmit) this.setState({value: ''}); 
+        if(this.props.clearOnSubmit) this.setState({value: ''});
     }
     render()
     {
         return(
             <div className="search-bar"  id="noselect">
                 <form onSubmit={this.handleSumbit}>
-                   <input type="text" autoComplete="off" placeholder={this.props.placeholder} size={this.props.size > 0 ? this.props.size : 15} className="fa fa-input input-field" id="search-bar" value={this.state.value} onChange={this.handleChange}/>
-                   <input type="button" value={"\uf057"}   className="search-clear fa" style={this.state.value === "" ? {opacity: "0"} : {opacity: "1"} } onClick={this.handleReset}/>
+                   <input type={"text"} value={this.state.value} autoComplete="off" placeholder={this.props.placeholder} size={this.props.size > 0 ? this.props.size : 15} className={`fa fa-input input-field ${this.props.name}`} id="search-bar" onChange={this.handleChange}/>
+                   <input type="button" value={"\uf057"}   className="search-clear fa" style={this.state.value === "" ? {opacity: "0"} : {opacity: "1"} } onClick={this.handleReset} tabIndex="-1"/>
                 </form>
             </div>
         )
